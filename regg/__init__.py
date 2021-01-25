a7915a5d3fe24206fa8cef36afea5ee24fc110cd
Liveira <ag080107@gmail.com>
2021-01-25 14:40:25 -0300

    first!

---
+++ regg/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding utf-8 -*-
+"""
+Regex Generator
+------------------
+A simple regex generator
+
+Copyright 2021 Liveira 
+license MIT
+
+ """
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(
+    name="regg",
+    version="1.0",
+    description="A regex generator",
+    author="Liveira"
+)
